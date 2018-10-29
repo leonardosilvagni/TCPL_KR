a24684f6f47b83ff6ec12587b4e41dd17811080c
Leonardo Silvagni <31442085+leonardosilvagni@users.noreply.github.com>
2018-10-29 21:59:15 +0100

    Update program_indexing.sh
    Let it index files from the home dir and get working dir from stdin

--- Chapter_1/program_indexing.sh
+++ Chapter_1/program_indexing.sh
@@ -4,13 +4,15 @@ echo "If the program does not behave correctly, you might have accidentally chan
 The file are structured with the following pattern: Line 1 // esx-x from K&R
 Line 2: // Author YEAR
 Line 3: // Goal: ..."
-#if [ -z "$1" ]
-#then
-#	Directory=$PWD
-#else
-#	Directory=$1
-#fi
-#cd $1
+Directory=/home/leonardo/TCPL_KR/
+if [ -z "$1" ]
+then
+    cd Directory
+else
+    cd Directory
+    cd $1
+fi
+echo "$PWD"
 for i in *.c; do
 	sed -n 1p $i #Print the name of the file
 	sed -n 3p $i #Print the Goal the program should acheive
