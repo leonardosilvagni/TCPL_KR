0c4e2c484b6d5c015f0d29ee35b4113cad00219b
Leonardo Silvagni <leonardo.silvagni@gmail.com>
2018-11-01 16:49:36 +0100

    Major bug fix

--- Chapter_1/es1-13.c
+++ Chapter_1/es1-13.c
@@ -4,10 +4,8 @@
 
 #include <stdio.h>
 
-const int MAX_WORD = 120;
 void print_x_istogram();
 void print_y_istogram();
-int word_count();
 int main(){
 	int question;
 	printf("h to print horizontal istogram OR v to print vertical istogram\n");
@@ -36,6 +34,38 @@ void print_x_istogram(){
 }
 
 void print_y_istogram(){
-		
+	int c,char_count=0,word_count=0,state=0,it=0;
+	int buf[1000];
+	while((c=getchar())!=EOF && it!=998){
+		it++;
+		char_count++;
+		buf[it]=c;	
+	};
+	buf[it+1]=EOF;
+	it=0;
+	while(buf[it]!=EOF){
+		if(buf[it]==' '||buf[it]=='\t'||buf[it]=='\n')state=0;
+		else if (state==0){
+			state=1;
+			word_count++;};
+	it++;
+	};
+	it=0;
+	int char_per_word[word_count];
+	while(buf[it]!=EOF){
+		if(buf[it]==' '||buf[it]=='\t'||buf[it]=='\n')state=0;
+		else if (state==0){
+			state=1;
+			char_per_word[it]++;};
+	it++;
+	};
+
 }
+
+
+
+
+
+
+
 
