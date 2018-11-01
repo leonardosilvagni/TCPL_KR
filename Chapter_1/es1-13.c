e2a3d0daae8b8f9fa309a3edb7b32a0935a9caa2
Leonardo Silvagni <leonardo.silvagni@gmail.com>
2018-11-01 16:09:40 +0100

    Started ex1-13

---
+++ Chapter_1/es1-13.c
@@ -0,0 +1,64 @@
+// es1-13.c from K&R
+// Leonardo Silvagni 2018
+// Goal: Print an Istogram of the lenght of words in its input
+
+#include <stdio.h>
+
+const int MAX_WORD = 120;
+void print_x_istogram();
+void print_y_istogram();
+int word_count();
+int main(){
+	int question;
+	printf("h to print horizontal istogram OR v to print vertical istogram\n");
+	switch(question=getchar()){
+		case 'h':
+		case 'H':
+			print_x_istogram();
+			return 0;
+		case 'v':
+		case 'V':
+			print_y_istogram();
+			return 0;
+		default:
+			print_y_istogram();
+			return 0;
+	};
+}
+
+void print_x_istogram(){
+	int c;
+	while((c=getchar())!=EOF){
+		if(c!=' '&&c!='\n'){
+			putchar('*');
+		}else putchar('\n');
+	};
+}
+
+void print_y_istogram(){
+	int buf[1000];
+	int state=0,wc=0,c;
+	while((c=getchar())!=EOF){
+		for(int it=0;it!=1000;it++){
+			if((buf[it]=c)=='\n') it=1000;
+			if (buf[it]==' '||buf[it]=='\t') state=0;
+			else if (state==0){
+				state=1;
+				wc++;
+			};
+		};
+		int word_buf[wc];
+		for(
+	};	
+}
+
+/*int word_count(){
+	int state=0,wc,c;
+	while((c=getchar())!=EOF){
+		if(c='\n') return wc;
+		if(c=' '||c='\t') state=0;
+		else if (state==0){
+		state=1;
+		wc++;}
+	}return wc;
+}*/
