d7cc013a969f054ccec38b9275a750a9763a0c68
Leonardo Silvagni <leonardo.silvagni@gmail.com>
2018-11-02 13:21:53 +0100

    Es1-13 verical does not work

--- Chapter_1/es1-13.c
+++ Chapter_1/es1-13.c
@@ -3,12 +3,11 @@
 // Goal: Print an Istogram of the lenght of words in its input
 // Error on char_per_word and word count, should format the input.
 #include <stdio.h>
-
 void print_x_istogram();
 void print_y_istogram();
 int main(){
 	int question;
-	printf("h to print horizontal istogram OR v to print vertical istogram\n");
+	printf("h to print horizontal istogram OR v to print vertical istogram(does not work)\n");
 	switch(question=getchar()){
 		case 'h':
 		case 'H':
@@ -19,7 +18,7 @@ int main(){
 			print_y_istogram();
 			return 0;
 		default:
-			print_y_istogram();
+			print_x_istogram();
 			return 0;
 	};
 }
@@ -35,14 +34,14 @@ void print_x_istogram(){
 
 void print_y_istogram(){
 	int c,max_char_count=0,word_count=0,state=0,it=0,char_count=0;
-	int buf[50];
-	while((c=getchar())!=EOF && it!=48){
+	int buf[50]={0};
+	while((c=getchar())!='\n' && it!=48){
 		it++;
 		buf[it]=c;	
 	};
-	buf[it+1]=EOF;
+	buf[49]=EOF;
 	it=0;
-	while(buf[it]!=EOF){
+	while(buf[it]!=EOF||buf[it]!='\n'){
 		if(buf[it]==' '||buf[it]=='\t'||buf[it]=='\n')state=0;
 		else if (state==0){
 			state=1;
@@ -56,11 +55,13 @@ void print_y_istogram(){
 	it=0;
 	int char_per_word[word_count];
 	for(int i=0; i<word_count;i++)char_per_word[i]=0;
+	state=0;
 	while(buf[it]!=EOF){
 		if(buf[it]==' '||buf[it]=='\t'||buf[it]=='\n')state=0;
 		else if (state==0){
 			state=1;
-			char_per_word[it]++;};
+			char_per_word[it]+=1
+		;};
 	it++;
 	};//The error is here according to GDB
 	putchar('\n');
@@ -68,7 +69,7 @@ void print_y_istogram(){
 		for(int j=0;j!=word_count;j++){
 			if(char_per_word[j]!=0){
 				putchar('*');
-				char_per_word[j]--;
+				char_per_word[j]-=1;
 			}else if(char_per_word[j]==0)putchar(' ');
 		};
 		putchar('\n');
