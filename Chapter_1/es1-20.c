c4a8fbd17bb1027fd51ed1ef50d50fd820c6c2ce
Leonardo Silvagni <leonardo.silvagni@gmail.com>
2018-11-11 17:46:57 +0100

    Ex1-20.c

--- Chapter_1/es1-20.c
+++ Chapter_1/es1-20.c
@@ -2,23 +2,25 @@
 // Leonardo Silvagni 2018
 // Goal: Replace the tabs with blanks
 #include <stdio.h>
-const int TABSTOP = 4;
+#define TABSTOP 4
 void detab();
 void entab();
 int main(){
-	
-
+	int input;
+	printf("%6d%6d%6d%6d",1%4,2%4,3%4,4%4);
+	printf("Input only a number: 0 to detab, 1 to entab.");
+	input=getchar();
+	if (input=='0')detab();
+	if (input=='1')entab();
+	return 0;
 }
 void detab(){
 	int c;
 	while((c=getchar())!=EOF){
-		switch(c){
-			case '\t':
-				for(int i=0;i>TABSTOP;++i)putchar(c);
-				break;
-			default:
+			if(c=='\t'){
+				for(int i=1;i<TABSTOP;i++)putchar(' ');
+			}else {
 				putchar(c);
-				break;
 		};
 	};
 }
@@ -36,11 +38,19 @@ void entab(){
 				if(previous_tabs!=0||previous_spaces!=0){
 					previous_spaces+=previous_tabs*TABSTOP; // Making a default spacing to handle unoptimized cases
 			// such as "\t \t \t \t "	that has 4 spaces not consec.
-					previous_tabs=0;// Reinit the unused variable
+					if(previous_spaces==TABSTOP)putchar('\t');
+					else if(previous_spaces<TABSTOP)
+						for(int i=0;i<previous_spaces;i++)putchar(' ');
 					// Now we must put the minimum spaces to be used in output to reach the same pattern
-					for(int i=0;i<=(previous_spaces%TABSTOP);++i)putchar(' ');
-
-				}
+					else if(previous_spaces>TABSTOP){
+						for(int i=0;i<(previous_tabs=(previous_spaces%TABSTOP));i++)putchar(' ');
+					
+					// Now we remove that from the spaces number, note that prev_tab is used for convenience
+					previous_spaces-=previous_tabs;
+					previous_tabs=previous_spaces/TABSTOP;// Reinit the unused variable
+					for(int i=0;i<=previous_tabs;i++)putchar('\t');}
+				};
+				putchar(c);
 		}
 	}
 }
