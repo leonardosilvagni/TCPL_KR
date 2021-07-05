f3036ee69edd68758d74a12e1e75800b5b4a4170
leonardosilvagni <leonardo.silvagni@gmail.com>
2021-07-05 21:12:41 +0200

    Prova VsCode 2

--- Chapter_1/es1-4.c
+++ Chapter_1/es1-4.c
@@ -5,7 +5,7 @@
 
 int main(){
 	float fahr, celsius;
-	const float lower= 0 , upper = 320, step = 20;
+	const float lower= 0 , upper = 340, step = 20;
 	fahr = lower;
 	printf("Fahrenheit\tCelsius\n");
 	while(fahr<=upper){
