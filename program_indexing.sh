5fca8b891ef22ce80ec9c04cf8d27d3e0d555808
leonardosilvagni <leonardo.silvagni@gmail.com>
2021-07-08 10:01:13 +0200

    Modified program_indexing.sh to accept no argumetn given

--- program_indexing.sh
+++ program_indexing.sh
@@ -4,18 +4,20 @@ echo "If the program does not behave correctly, you might have accidentally chan
 The file are structured with the following pattern: Line 1 // esx-x from K&R
 Line 2: // Author YEAR
 Line 3: // Goal: ..."
-Directory=/home/leonardo/Work/TCPL_KR/
+Directory=.
 if [ -z "$1" ]
 then
-    cd $Directory
+	cd $Directory
+	echo "Error"
+	echo "Expected argument to be the number of the Chapter to look for files..."
 else
     cd $Directory
     cd Chapter_[$1]
-fi
 echo "$PWD"
-for i in *.c; do
-	sed -n 1p $i #Print the name of the file
-	sed -n 3p $i #Print the Goal the program should acheive
-	echo 
-done
+	for i in *.c; do
+		sed -n 1p $i #Print the name of the file
+		sed -n 3p $i #Print the Goal the program should acheive
+		echo 
+	done
+fi
 exit 0
